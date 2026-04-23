e67b619dc5487f46f0f603796e99f3d5b4d238a1
ramistodev <168463169+ramistodev@users.noreply.github.com>
2026-04-23 10:08:06 +0200

    maybe now deploy

--- backend-js/src/app/lib/email/resend.config.ts
+++ backend-js/src/app/lib/email/resend.config.ts
@@ -1,4 +1,4 @@
-import { resend_api_key } from "@/app/config/envConfig";
+import { resend_api_key } from "../../config/envConfig";
 import { Resend } from "resend";
 
 export const resendClient = new Resend(resend_api_key);
