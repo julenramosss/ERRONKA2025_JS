389b20d33cc4d7a6dc04d1fa43c0b1425967fcb7
ramistodev <168463169+ramistodev@users.noreply.github.com>
2026-04-30 10:50:37 +0200

    Fix Users entering

--- frontend-app/app/(main)/components/AsideMenu/components/MenuOptions.tsx
+++ frontend-app/app/(main)/components/AsideMenu/components/MenuOptions.tsx
@@ -2,6 +2,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Icons } from '../../../../components/icons';
+import { USER_ROLES } from '../../../../utils/types';
+import { useMe } from '../../../../hooks/auth/useMe';
 
 export function MenuOptions({
   isMinimized,
@@ -11,36 +13,47 @@ export function MenuOptions({
   onClose?: () => void;
 }) {
   const pathname = usePathname();
+  const { data: user } = useMe();
 
   const items = [
-    { href: '/dashboard', label: 'Dashboard', icon: <Icons.Home size={20} /> },
+    {
+      href: '/dashboard',
+      label: 'Dashboard',
+      icon: <Icons.Home size={20} />,
+      allowUsers: [USER_ROLES.distributor],
+    },
     {
       href: '/myPackages',
       label: 'Nire Paketeak',
       icon: <Icons.Package size={20} />,
+      allowUsers: [USER_ROLES.distributor],
     },
     {
       href: '/myRoute',
       label: 'Nire Bidea',
       icon: <Icons.Route size={20} />,
+      allowUsers: [USER_ROLES.distributor],
     },
     {
       href: '/history',
       label: 'Historia',
       icon: <Icons.History size={20} />,
+      allowUsers: [USER_ROLES.distributor],
     },
     {
       href: '/settings',
       label: 'Ezarpenak',
       icon: <Icons.Settings size={20} />,
+      allowUsers: [USER_ROLES.distributor, USER_ROLES.admin],
     },
   ];
 
   return (
     <div>
       <ul className="flex flex-col gap-2 text-text-secondary">
-        {items.map(({ href, label, icon }) => {
+        {items.map(({ href, label, icon, allowUsers }) => {
           const isActive = pathname === href;
+          const isAllowed = user && allowUsers.includes(user.role);
           return (
             <li
               key={href}
@@ -55,7 +68,7 @@ export function MenuOptions({
                 href={href}
                 className={`flex text-base font-semibold items-center gap-3 py-2 ${
                   isMinimized ? 'justify-center px-2' : 'px-4'
-                } ${isActive ? 'cursor-default pointer-events-none' : ''}`}
+                } ${isActive ? 'cursor-default pointer-events-none' : ''} ${!isAllowed ? 'pointer-events-none opacity-50' : ''}`}
                 title={isMinimized ? label : undefined}
               >
                 {icon}
